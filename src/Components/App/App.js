203492c92853430dcc1c6a101aa9091d454fb83e
JacobDChamberlain <straightlegmusic@gmail.com>
2023-12-18 23:56:30 -0600

    get the Access Token from URL hash (#)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -7,12 +7,30 @@ function App() {
   const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
   const RESPONSE_TYPE = 'token';
 
+  const authenticationURL = `${ AUTH_ENDPOINT }?client_id=${ CLIENT_ID }&redirect_uri=${ REDIRECT_URI }&response_type=${ RESPONSE_TYPE }`;
+
+  const [ token, setToken ] = useState('');
+
+  useEffect( () => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem( 'token' );
+
+    if ( !token && hash ) {
+      token = hash.substring(1).split('&').find( elem => elem.startsWith( 'access_token' ) ).split('=')[1];
+
+      window.location.hash = '';
+      window.localStorage.setItem( 'token', token );
+    }
+
+    setToken( token );
+  }, [] );
+
   return (
     <div className="App">
       <header className='app-header'>
 
       </header>
-      <a href={ `${ AUTH_ENDPOINT }?client_id=${ CLIENT_ID }&redirect_uri=${ REDIRECT_URI }&response_type=${ RESPONSE_TYPE }` }>
+      <a href={ authenticationURL }>
         Login to Spotify
       </a>
     </div>
